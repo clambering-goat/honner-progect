811d7fb20eb04b93146853792a91022ae9784f01
Cameron Drain <camerondrain@gmail.com>
2019-01-30 17:03:11 +0000

    add 3d mondel

---
+++ model_to_point_cloud/g_code_to_point_cloud_clean.py
@@ -0,0 +1,14 @@
+import graphics
+
+
+
+
+class draw():
+    def __init__(self):
+        win = graphics.GraphWin(width=200,height=200)
+
+    def points_to_draw(x,y)
+        pix=Point(x,y)
+        pix.draw(win)
+
+    def
