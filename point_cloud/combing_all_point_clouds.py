057547a5d968484e734365fee59d1a4c66c09e47
Cameron Drain <camerondrain@gmail.com>
2019-02-06 17:06:44 +0000

    fix bug in rotation code

---
+++ point_cloud/combing_all_point_clouds.py
@@ -0,0 +1,28 @@
+import numpy as np
+from math import sin cos
+
+data1=np.load("file1.npy")
+data2=np.load("file2.npy")
+data3=np.load("file3.npy")
+
+
+main_data=[data1,data2,data3]
+file=open("kinect_point_cloud.xyz","w")
+
+max_depth_vaule=255
+
+angel=0
+for data in main_data:
+    angel=angel+60
+    x_p=-1
+    y_p=-1
+    for y in data:
+        y_p+=1
+        x_p=-1
+        for z_data in y:
+            x_p+=1
+            transpotion=255-z_data
+            data=str(x_p)+" "+str(y_p)+" "+str(transpotion)+" \n"
+            file.write(data)
+
+file.close()
