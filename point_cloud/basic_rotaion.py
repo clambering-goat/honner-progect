bd71dfc6d0ad9d3b9e214a4da72f9b5d45b09cee
Cameron Drain <camerondrain@gmail.com>
2019-02-08 19:28:55 +0000

    scan and point cloudes

--- point_cloud/basic_rotaion.py
+++ point_cloud/basic_rotaion.py
@@ -4,8 +4,8 @@ from math import sin,cos,radians
 base_points=[]
 
 z_points=20
-for x_point in range(2):
-    for y_point in range(2):
+for x_point in range(0,6):
+    for y_point in range(0,6):
         base_points.append((x_point*2,y_point*2,z_points*2))
 
 
@@ -15,23 +15,25 @@ for x_point in range(2):
 
 
 # rotaion points
-angle=120
-angle=radians(angle)
-s=sin(angle)
-c=cos(angle)
-
-print(s)
-print(c)
 new_points_to_add=[]
-for loop in base_points:
-    x=loop[0]
-    y=loop[1]
-    z=loop[2]
-    #math from roation
-    x1=(x*c)-(y*s)
-    y1=(x*s)+(y*c)
+for loop1 in range(0,360,10):
+    angle=loop1
+    angle=radians(angle)
+    s=sin(angle)
+    c=cos(angle)
 
-    new_points_to_add.append((x1,y1,z))
+    print(s)
+    print(c)
+
+    for loop in base_points:
+        x=loop[0]
+        y=loop[1]
+        z=loop[2]
+        #math from roation
+        x1=(x*c)-(y*s)
+        y1=(x*s)+(y*c)
+
+        new_points_to_add.append((x1,y1,z))
 
 
 
@@ -56,12 +58,12 @@ for w in new_points_to_add:
         c = a ** 2 + b ** 2
         c=c**0.5
 
-        a=base_points[x_count][0]-base_points[y_count][0]
-        b=base_points[x_count][1]-base_points[y_count][1]
-        c2 = (a ** 2) + (b ** 2)
-        c2=(c2**0.5)
+        #a=base_points[x_count][0]-base_points[y_count][0]
+        #b=base_points[x_count][1]-base_points[y_count][1]
+        #c2 = (a ** 2) + (b ** 2)
+        #c2=(c2**0.5)
 
-        print("start ",c2," end ",c," ",x_count,y_count)
+        #print("start ",c2," end ",c," ",x_count,y_count)
 
 
 
