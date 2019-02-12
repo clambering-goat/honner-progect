acc2ab9328020d8296239ff1a2b714a9c84c2e81
Cameron Drain <camerondrain@gmail.com>
2019-02-12 04:56:19 +0000

    fined caliable code for 2 kinect
    only for 2 kinect need cahnged to work with 3
    
    need teast in large room to make sure result are good enough

--- kinect_code/2_kinect_code/temp.py
+++ kinect_code/2_kinect_code/temp.py
@@ -4,15 +4,7 @@
 
 
 
-def big_swap(v1,v2):
-    if v2 < v1:
-        temp=v1
-        v1=v2
-        v2=temp
-    return(v1,v2)
 
 
-v1,v2=big_swap(-50,500)
-
-for q in range (v1,v2):
+for q in range (-50,0):
     print(q)
